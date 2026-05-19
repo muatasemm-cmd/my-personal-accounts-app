a653b5f903dc003b796af2e01e80727a695d052d
muatasemm-cmd <muatasemm@gmail.com>
2026-05-19 09:33:17 +0300

    Add files via upload

---
+++ site-v2.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    if (!("serviceWorker" in navigator)) {
+        return;
+    }
+
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+            registrations.forEach(function (registration) {
+                registration.unregister();
+            });
+        }).catch(function () {
+        });
+
+        if (window.location.search.indexOf("reset=1") >= 0) {
+            if (window.caches && caches.keys) {
+                caches.keys().then(function (keys) {
+                    keys.forEach(function (key) {
+                        caches.delete(key);
+                    });
+                }).catch(function () {
+                });
+            }
+        }
+    });
+})();
